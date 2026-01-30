75e292d47a0a8ddffa5fafadc3fcae0900ae3d39
Alexandre Schönwitz <alexandre.schoenwitz@freiheit.com>
2026-01-30 19:37:30 +0000

    api

--- server/main.go
+++ server/main.go
@@ -5,7 +5,7 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/alexschoenwitz/mask-invaders/server/api" // Your generated code
+	"github.com/alexschoenwitz/mask-invaders/api/server/api"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -13,18 +13,18 @@ import (
 
 // --- The gRPC Server ---
 type server struct {
-	api.Unim
+	api.UnimplementedServerServer
 }
 
-func (s *server) Echo(ctx context.Context, req *api.EchoRequest) (*api.EchoResponse, error) {
-	return &api.EchoResponse{Greeting: "Hello " + req.Name}, nil
+func (s *server) Echo(ctx context.Context, req *api.GetStateRequest) (*api.GetStateResponse, error) {
+	return &api.GetStateResponse{Greeting: "Hello"}, nil
 }
 
 func main() {
 	// 1. Start gRPC Server (Binary)
 	lis, _ := net.Listen("tcp", ":9090")
 	s := grpc.NewServer()
-	api.RegisterEchoServiceServer(s, &server{})
+	api.RegisterServerServer(s, &server{})
 	go s.Serve(lis)
 
 	// 2. Start Gateway Proxy (JSON -> gRPC)
@@ -36,7 +36,7 @@ func main() {
 	)
 
 	gwmux := runtime.NewServeMux()
-	api.RegisterEchoServiceHandler(context.Background(), gwmux, conn)
+	api.RegisterServerHandler(context.Background(), gwmux, conn)
 
 	// Serve the JSON API on port 8080
 	http.ListenAndServe(":8080", gwmux)
