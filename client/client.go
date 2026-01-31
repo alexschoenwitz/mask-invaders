9ecf69cd09ca0b859484703d31bb163304bcbbcd
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2026-01-31 00:34:14 +0000

    refactor client out of the dumbbot

---
+++ client/client.go
@@ -0,0 +1,144 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/alexschoenwitz/mask-invaders/api/server/api"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+// Client simplifies access to the HTTP API of the Mask Invaders server
+type Client struct {
+	baseURL    string
+	httpClient *http.Client
+
+	token    string
+	playerID string
+}
+
+// NewClient creates a new Client instance
+func NewClient(baseURL string) *Client {
+	return &Client{
+		baseURL:    baseURL,
+		httpClient: &http.Client{Timeout: 10 * time.Second},
+	}
+}
+
+func (c *Client) Register(name string) (string, error) {
+	reqBody := &api.RegisterRequest{Name: name}
+	jsonData, err := protojson.Marshal(reqBody)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(c.baseURL+"/v1/register", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("registration failed with status %d: %s", resp.StatusCode, string(body))
+	}
+
+	var regResp api.RegisterResponse
+	if err := protojson.Unmarshal(body, &regResp); err != nil {
+		return "", err
+	}
+	c.token = regResp.Token
+	c.playerID = regResp.Id
+	fmt.Printf("got token: %v\n", c.token)
+
+	return regResp.Id, nil
+}
+
+func (c *Client) StartGame() error {
+	req, err := http.NewRequest("POST", c.baseURL+"/v1/start", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("authorization", c.token)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		v := make(map[string]string)
+		_ = json.Unmarshal(body, &v)
+		if v["message"] == "game already started" {
+			return nil // good!
+		}
+		return fmt.Errorf("start game failed with status %d: %s", resp.StatusCode, string(body))
+	}
+	return nil
+}
+
+func (c *Client) GetState() (*api.State, error) {
+	req, err := http.NewRequest("GET", c.baseURL+"/v1/state", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("authorization", c.token)
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get state failed with status %d: %s", resp.StatusCode, string(body))
+	}
+
+	var stateResp api.GetStateResponse
+	if err := protojson.Unmarshal(body, &stateResp); err != nil {
+		return nil, err
+	}
+
+	return stateResp.State, nil
+}
+
+func (c *Client) PostAction(action *api.PostActionRequest) error {
+	jsonData, err := protojson.Marshal(action)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", c.baseURL+"/v1/action", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("authorization", c.token)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("post action failed with status %d: %s", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
--- client/dumb-bot/main.go
+++ client/dumb-bot/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"math"
-	"net/http"
 	"time"
 
 	"github.com/alexschoenwitz/mask-invaders/api/server/api"
-	"google.golang.org/protobuf/encoding/protojson"
+	"github.com/alexschoenwitz/mask-invaders/client"
 )
 
 const (
@@ -21,19 +17,19 @@ const (
 
 func main() {
 	// 1. Register the bot
-	token, playerID, err := register()
+	c := client.NewClient(serverURL)
+	playerID, err := c.Register(botName)
 	if err != nil {
 		log.Fatalf("Failed to register: %v", err)
 	}
-	log.Printf("Registered with token: %s, playerID: %s", token, playerID)
+	log.Printf("Registered with playerID: %s", playerID)
 
 	// 2. Try to start the game for 2 minutes
-	client := &http.Client{Timeout: 10 * time.Second}
 	gameStarted := false
 	startTime := time.Now()
 
 	for time.Since(startTime) < 2*time.Minute {
-		err := startGame(client, token)
+		err := c.StartGame()
 		if err == nil {
 			log.Println("Game started successfully!")
 			gameStarted = true
@@ -49,68 +45,16 @@ func main() {
 	}
 
 	// 3. Play the game with the dumb strategy
-	playGame(client, token, playerID)
+	playGame(c, playerID)
 }
 
-func register() (string, string, error) {
-	reqBody := &api.RegisterRequest{Name: botName}
-	jsonData, err := protojson.Marshal(reqBody)
-	if err != nil {
-		return "", "", err
-	}
-
-	resp, err := http.Post(serverURL+"/v1/register", "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return "", "", err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		return "", "", fmt.Errorf("registration failed with status %d: %s", resp.StatusCode, string(body))
-	}
-
-	var regResp api.RegisterResponse
-	if err := protojson.Unmarshal(body, &regResp); err != nil {
-		return "", "", err
-	}
-
-	return regResp.Token, regResp.Id, nil
-}
-
-func startGame(client *http.Client, token string) error {
-	req, err := http.NewRequest("POST", serverURL+"/v1/start", nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("authorization", token)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		v := make(map[string]string)
-		_ = json.Unmarshal(body, &v)
-		if v["message"] == "game already started" {
-			return nil // good!
-		}
-		return fmt.Errorf("start game failed with status %d: %s", resp.StatusCode, string(body))
-	}
-
-	return nil
-}
-
-func playGame(client *http.Client, token, playerID string) {
+func playGame(client *client.Client, playerID string) {
 	log.Println("Starting game loop...")
 
 	for {
 		time.Sleep(1 * time.Second) // Give some time between turns
 
-		state, err := getGameState(client, token)
+		state, err := client.GetState()
 		if err != nil {
 			log.Printf("Failed to get game state: %v", err)
 			continue
@@ -152,37 +96,38 @@ func playGame(client *http.Client, token, playerID string) {
 			continue
 		}
 
-		err = attack(client, token, playerID, attackFrom, attackTo, city.Troops)
+		noTroops := true
+		for _, ammount := range city.Troops {
+			if ammount > 0 {
+				noTroops = false
+				break
+			}
+		}
+
+		action := &api.PostActionRequest{
+			Action: &api.Action{
+				Player: playerID,
+				Action: &api.Action_Attack{
+					Attack: &api.Attack{
+						From:   attackFrom,
+						To:     attackTo,
+						Troops: city.Troops,
+					},
+				},
+			},
+		}
+
+		if noTroops {
+			fmt.Println("No troops to attack with")
+			action.Action.Action = &api.Action_None{}
+		}
+
+		err = client.PostAction(action)
 		if err != nil {
-			log.Printf("Failed to attack: %v", err)
+			log.Printf("Failed to post action: %v", err)
 			continue
 		}
 	}
-}
-
-func getGameState(client *http.Client, token string) (*api.State, error) {
-	req, err := http.NewRequest("GET", serverURL+"/v1/state", nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("authorization", token)
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("get state failed with status %d: %s", resp.StatusCode, string(body))
-	}
-
-	var stateResp api.GetStateResponse
-	if err := protojson.Unmarshal(body, &stateResp); err != nil {
-		return nil, err
-	}
-
-	return stateResp.State, nil
 }
 
 func findNearestAttack(myCities, enemyCities []string, state *api.State) (string, string) {
@@ -212,58 +157,4 @@ func getDistance(city1, city2 string, distances map[string]*api.Distance) int64 
 		return dist.Distance
 	}
 	return math.MaxInt64 // No connection found
-}
-
-func attack(client *http.Client, token, playerID, from, to string, troops map[string]int64) error {
-	action := &api.PostActionRequest{
-		Action: &api.Action{
-			Player: playerID,
-			Action: &api.Action_Attack{
-				Attack: &api.Attack{
-					From:   from,
-					To:     to,
-					Troops: troops,
-				},
-			},
-		},
-	}
-
-	noTroops := true
-	for _, ammount := range troops {
-		if ammount > 0 {
-			noTroops = false
-			break
-		}
-	}
-	if noTroops {
-		fmt.Println("No troops to attack with")
-		action.Action.Action = &api.Action_None{}
-	} else {
-		log.Printf("Will attack from %s to %s with troops %+v", from, to, troops)
-	}
-
-	jsonData, err := protojson.Marshal(action)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", serverURL+"/v1/action", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("authorization", token)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("attack failed with status %d: %s", resp.StatusCode, string(body))
-	}
-
-	return nil
 }
